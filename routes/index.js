d7fa7c65ec990968a34431dc6b81e1f91fa40c93
Jarom Edwards <jaromjedwards@gmail.com>
2024-01-15 14:19:45 -0700

    j

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const routes = require('express').Router();
 const contacts = require('./contacts');
 
 routes.get('/', (req, res) => (res.send('Hello World')));
-
 routes.use('/contacts', contacts);
 
 module.exports = routes;
