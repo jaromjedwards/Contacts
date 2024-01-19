144c44d3023ba17b64215f75dc47952eb5765c57
Jarom Edwards <jaromjedwards@gmail.com>
2024-01-18 17:28:35 -0700

    failed

--- swagger.js
+++ swagger.js
@@ -3,12 +3,7 @@ const swaggerAutogen = require('swagger-autogen');
 const doc = {
     info: {
         title: "Users Api",
-        description: "Users Api",
-        contact: {
-            name: "Your Name",
-            email: "your.email@example.com",
-            url: "https://www.example.com"
-        }
+        description: "Users Api"
     },
     host: 'localhost:3000',
     schemes: ['https', 'http']
@@ -17,4 +12,4 @@ const doc = {
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
-swaggerAutogen(outputFile, endpointsFiles, doc);
+swaggerAutogen(outputFile, endpointsFiles, doc)
